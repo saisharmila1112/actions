50facc53cad0854c7d413911f07c92225f6c30fe
saisharmila1112 <rednamsaisharmila@gmail.com>
2020-09-14 17:19:35 +0200

    initial commit

---
+++ precache-manifest.e865b894489cd91ff37c0d29f0298bf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8113290de0b1b931220b4fb6ad2ad52d",
+    "url": "/actions/index.html"
+  },
+  {
+    "revision": "c2727887e723fbdb9e3f",
+    "url": "/actions/static/css/main.209b399c.chunk.css"
+  },
+  {
+    "revision": "ff3ebb6e143dcd4bacb0",
+    "url": "/actions/static/js/2.cda03690.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/actions/static/js/2.cda03690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2727887e723fbdb9e3f",
+    "url": "/actions/static/js/main.6f943e03.chunk.js"
+  },
+  {
+    "revision": "389de14c16024f1e3ea4",
+    "url": "/actions/static/js/runtime-main.bb380df8.js"
+  }
+]);
